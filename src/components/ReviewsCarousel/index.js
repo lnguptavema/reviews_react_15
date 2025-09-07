03329e8a5197e2a6e89e8d13b438c25887354490
lnguptavema <nareshgupta2294@gmail.com>
2025-09-07 10:56:55 +0000

    first commit

---
+++ src/components/ReviewsCarousel/index.js
@@ -0,0 +1,65 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class ReviewsCarousel extends Component {
+  state = {count: 0}
+
+  clickedLeftImg = () => {
+    const {count} = this.state
+    if (count !== 0) {
+      this.setState({count: count - 1})
+    }
+  }
+
+  clickedRightImg = () => {
+    const {count} = this.state
+    if (count < 3) {
+      this.setState({count: count + 1})
+    }
+  }
+
+  render() {
+    const {reviewsList} = this.props
+    const {count} = this.state
+    return (
+      <div className="mainContainer">
+        <h1>Reviews</h1>
+        <div className="cardItem">
+          <ul>
+            <li>
+              <div className="miniCard">
+                <button data-testId="leftArrow" type="button">
+                  <img
+                    onClick={this.clickedLeftImg}
+                    src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
+                    alt="left arrow"
+                    className="arrow"
+                  />{' '}
+                </button>
+                <img
+                  src={reviewsList[count].imgUrl}
+                  alt={reviewsList[count].username}
+                />
+
+                <button data-testId="rightArrow" type="button">
+                  {' '}
+                  <img
+                    onClick={this.clickedRightImg}
+                    src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
+                    alt="right arrow"
+                    className="arrow"
+                  />{' '}
+                </button>
+              </div>
+              <p>{reviewsList[count].companyName}</p>
+              <p className="description">{reviewsList[count].description} </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ReviewsCarousel
